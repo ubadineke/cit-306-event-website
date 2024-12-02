c6fa051131d280dd8487a9dc57bd8f2c3ae187f4
ubadineke <ubadinekeprince@gmail.com>
2024-12-02 20:39:36 +0100

    activated cors

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
